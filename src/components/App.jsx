97c8e6eadb51c0c80f4af3536eb2fb7191d9ab1f
user name <106239754+Anatoliy-Bolyukh@users.noreply.github.com>
2022-09-14 13:57:27 +0300

    add fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import Button from "./Button/Button";
 import Section from "./Section/Section";
 import Statistics from "./Statistics/Statistics";
